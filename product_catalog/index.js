132a4de7c4951a67f42d805c5b0ca716078dc152
Damy Metzke <damy@brightboost.nl>
2023-07-18 12:03:36 +0200

    Add `product_catalog` service

---
+++ product_catalog/index.js
@@ -0,0 +1,16 @@
+import express from 'express';
+
+const app = express();
+const port = 8080;
+
+app.get('/products', (_, res) => {
+  res.send({message: "Hello from the microservice"});
+});
+
+app.get('/products/:id', (req, res) => {
+  res.send({message: `Hello from the microservice, id = ${req.params.id}`});
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});

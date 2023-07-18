39a3d189b0b83d7a1d59a5d239a6daad1d5d1aff
Damy Metzke <damy@brightboost.nl>
2023-07-18 14:27:39 +0200

    Add users to `user_management` service

--- user_management/src/main.rs
+++ user_management/src/main.rs
@@ -1,13 +1,42 @@
-use actix_web::{get, web, App, HttpServer, Responder, HttpRequest};
+use actix_web::{get, web, App, HttpRequest, HttpServer, Responder};
+use serde::Serialize;
+
+#[derive(Serialize)]
+struct User {
+    id: i32,
+    name: &'static str,
+    email: &'static str,
+}
+
+const USERS: [User; 3] = [
+    User {
+        id: 0,
+        name: "Alice",
+        email: "alice@example.com",
+    },
+    User {
+        id: 1,
+        name: "Bob",
+        email: "bob@example.com",
+    },
+    User {
+        id: 2,
+        name: "Charly",
+        email: "charly@example.com",
+    },
+];
 
 #[get("/users")]
 async fn list_users() -> impl Responder {
-    "Hello users!"
+    serde_json::to_string(&USERS)
 }
 
 #[get("/users/{id}")]
 async fn get_user(id: web::Path<i32>) -> impl Responder {
-    format!("Hello user {id}!")
+    match USERS.iter().find(|user| user.id == *id) {
+        Some(user) => serde_json::to_string(user),
+        None => Ok(String::new()),
+    }
 }
 
 #[get("*")]

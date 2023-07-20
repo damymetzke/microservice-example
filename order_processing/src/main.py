21eab42e0c8f3859776854d6541e3c0d75033671
Damy Metzke <damy@brightboost.nl>
2023-07-20 23:28:31 +0200

    Implement cart for orders

--- order_processing/src/main.py
+++ order_processing/src/main.py
@@ -13,8 +13,18 @@ def list_orders():
 
     for order in orders:
         user = requests.get(f'http://user-management/users/{order["user_id"]}')
+        cart = []
+
+        for cart_item in order["cart"]:
+            product = requests.get(f'http://product-catalog/products/{cart_item["product_id"]}')
+            cart.append({
+                "product": product.json(),
+                "amount": cart_item["amount"]
+            })
+
         result.append({
             "user": user.json(),
+            "cart": cart,
         })
 
     return {"orders": result}
@@ -35,9 +45,27 @@ def create_order():
     if user_response.status_code == 404:
         return jsonify({'message': 'User not found'}), 404
 
+    stored_cart = []
+
+    for product in cart:
+        product_id = product.get('product_id')
+        amount = product.get('amount')
+
+        if product_id is None or amount is None:
+            return jsonify({'message': 'Missing required fields'}), 400
+
+        product_response = requests.get(f'http://user-management/users/{product_id}')
+        if product_response.status_code == 404:
+            return jsonify({'message': 'Product not found'}), 404
+
+        stored_cart.append({
+            "product_id": product_id,
+            "amount": amount,
+        })
+
     order = {
         'user_id': user_id,
-        'cart': cart
+        'cart': stored_cart
     }
     orders.append(order)
     return ""

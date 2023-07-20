46198b97c702fbdb17af3cb804e0b0f586ceb3bf
Damy Metzke <damy@brightboost.nl>
2023-07-20 23:33:50 +0200

    Refactor away get requests for api

--- api/main.go
+++ api/main.go
@@ -8,8 +8,8 @@ import (
 	"github.com/gorilla/mux"
 )
 
-func list_products(w http.ResponseWriter, req *http.Request) {
-  response, err := http.Get("http://product-catalog/products/")
+func make_get_request(w http.ResponseWriter, url string) {
+  response, err := http.Get(url)
 	if err != nil {
 		// Handle the error appropriately
 		http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -33,60 +33,18 @@ func list_products(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write(body)
+}
+
+func list_products(w http.ResponseWriter, req *http.Request) {
+  make_get_request(w, "http://product-catalog/products/")
 }
 
 func list_users(w http.ResponseWriter, req *http.Request) {
-  response, err := http.Get("http://user-management/users")
-	if err != nil {
-		// Handle the error appropriately
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	defer response.Body.Close()
-
-	if response.StatusCode != http.StatusOK {
-		// Handle the non-OK status code appropriately
-		http.Error(w, fmt.Sprintf("Server returned non-OK status: %v", response.StatusCode), http.StatusInternalServerError)
-		return
-	}
-
-	body, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		// Handle the error appropriately
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write(body)
+  make_get_request(w, "http://user-management/users")
 }
 
 func list_orders(w http.ResponseWriter, req *http.Request) {
-  response, err := http.Get("http://order-processing/orders")
-	if err != nil {
-		// Handle the error appropriately
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	defer response.Body.Close()
-
-	if response.StatusCode != http.StatusOK {
-		// Handle the non-OK status code appropriately
-		http.Error(w, fmt.Sprintf("Server returned non-OK status: %v", response.StatusCode), http.StatusInternalServerError)
-		return
-	}
-
-	body, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		// Handle the error appropriately
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write(body)
+  make_get_request(w, "http://order-processing/orders")
 }
 
 func create_order(w http.ResponseWriter, req *http.Request) {
